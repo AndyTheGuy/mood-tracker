8bb072b270c1629911b3defe2f7eb3cb9c455220
AndyTheGuy <44274852+AndyTheGuy@users.noreply.github.com>
2025-11-26 23:40:16 +0200

    Fix OneSignal initialization configuration

--- public/onesignal-config.js
+++ public/onesignal-config.js
@@ -1,9 +1,6 @@
 window.OneSignalDeferred = window.OneSignalDeferred || [];
 OneSignalDeferred.push(async function(OneSignal) {
   await OneSignal.init({
-    appId: "bf91095e-0266-40c4-9e9f-5faa720b263d",
-    serviceWorkerPath: '/OneSignalSDKWorker.js',
-    serviceWorkerParam: { scope: '/' },
-    allowLocalhostAsSecureOrigin: true
+    appId: "bf91095e-0266-40c4-9e9f-5faa720b263d"
   });
 });
