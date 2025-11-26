796805cb47ff63b7a8347a730610ea266fb2d29e
AndyTheGuy <44274852+AndyTheGuy@users.noreply.github.com>
2025-11-26 23:29:25 +0200

    Reorder OneSignal configuration parameters

--- public/onesignal-config.js
+++ public/onesignal-config.js
@@ -2,11 +2,8 @@ window.OneSignalDeferred = window.OneSignalDeferred || [];
 OneSignalDeferred.push(async function(OneSignal) {
   await OneSignal.init({
     appId: "bf91095e-0266-40c4-9e9f-5faa720b263d",
-    notifyButton: {
-      enable: false,
-    },
-    allowLocalhostAsSecureOrigin: true,
     serviceWorkerPath: '/OneSignalSDKWorker.js',
-    serviceWorkerParam: { scope: '/' }
+    serviceWorkerParam: { scope: '/' },
+    allowLocalhostAsSecureOrigin: true
   });
 });
